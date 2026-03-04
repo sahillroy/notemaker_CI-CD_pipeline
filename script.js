4b4b3c353fee1ced198f45192058e43a70df3806
Sahil Roy <roysp@rknec.edu>
2026-03-05 00:12:32 +0530

    Modified code

--- script.js
+++ script.js
@@ -1,17 +1,64 @@
-function addTask() {
-    const input = document.getElementById("taskInput");
-    const taskText = input.value;
+const btnEl = document.getElementById("btn");
+const appEl = document.getElementById("app");
 
-    if (taskText === "") return;
+getNotes().forEach((note) => {
+  const noteEl = createNoteEl(note.id, note.content);
+  appEl.insertBefore(noteEl, btnEl);
+});
 
-    const li = document.createElement("li");
-    li.textContent = taskText;
+function createNoteEl(id, content) {
+  const element = document.createElement("textarea");
+  element.classList.add("note");
+  element.placeholder = "Empty Note";
+  element.value = content;
 
-    li.onclick = function () {
-        li.remove();
-    };
+  element.addEventListener("dblclick", () => {
+    const warning = confirm("Do you want to delete this note?");
+    if (warning) {
+      deleteNote(id, element);
+    }
+  });
 
-    document.getElementById("taskList").appendChild(li);
+  element.addEventListener("input", () => {
+    updateNote(id, element.value);
+  });
 
-    input.value = "";
+  return element;
 }
+
+function deleteNote(id, element) {
+    const notes = getNotes().filter((note)=>note.id != id)
+    saveNote(notes)
+    appEl.removeChild(element)
+}
+
+function updateNote(id, content) {
+  const notes = getNotes();
+  const target = notes.filter((note) => note.id == id)[0];
+  target.content = content;
+  saveNote(notes);
+}
+
+function addNote() {
+  const notes = getNotes();
+  const noteObj = {
+    id: Math.floor(Math.random() * 100000),
+    content: "",
+  };
+  const noteEl = createNoteEl(noteObj.id, noteObj.content);
+  appEl.insertBefore(noteEl, btnEl);
+
+  notes.push(noteObj);
+
+  saveNote(notes);
+}
+
+function saveNote(notes) {
+  localStorage.setItem("note-app", JSON.stringify(notes));
+}
+
+function getNotes() {
+  return JSON.parse(localStorage.getItem("note-app") || "[]");
+}
+
+btnEl.addEventListener("click", addNote);
